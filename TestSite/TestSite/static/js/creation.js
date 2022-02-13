ba90ac0ab35cb608f1e7f724a42ad4866434c1d0
grifondopala <grifondopala@gmail.com>
2022-02-13 16:40:46 +0300

    поставил ковычки (баг телеги)

--- TestSite/TestSite/static/js/creation.js
+++ TestSite/TestSite/static/js/creation.js
@@ -10,11 +10,11 @@ var i = 1;
 function AddQuestion(){
     let new_question = document.createElement('div');
     new_question.classList.add('question');
-    new_question.id = question${i};
-    var type_question = type_question${i};
+    new_question.id = `question${i}`;
+    var type_question = `type_question${i}`;
     new_question.innerHTML = generate_question_container(type_question)
 
-    const radioButtons = new_question.querySelectorAll(input[name=${type_question}]);
+    const radioButtons = new_question.querySelectorAll(`input[name=${type_question}]`);
 
     for(const radioButton of radioButtons){
         radioButton.addEventListener('change', function (){
@@ -24,19 +24,19 @@ function AddQuestion(){
 
     function new_question_container(id, value) {
         let question_container = document.querySelector('#'+id);
-        var a = question_container.querySelector(#${id}_container);
+        var a = question_container.querySelector(`#${id}_container`);
         if (a) question_container.removeChild(a);
         let question_info = document.createElement('div');
-        question_info.id = ${id}_container;
+        question_info.id = `${id}_container`;
         question_info.innerHTML = generate_question_info(id, value);
         question_container.appendChild(question_info);
         question_info.querySelector('.new_field').addEventListener('click', function(){ add_answer(id, value) });
     }
 
     function add_answer(id, value){
-        let answers_box = document.getElementById(${id}_answers);
+        let answers_box = document.getElementById(`${id}_answers`);
         let new_answer = document.createElement('p');
-        new_answer.innerHTML = value == "Выбор" ? <input> <input type="checkbox"> : <input>;
+        new_answer.innerHTML = value == "Выбор" ? `<input> <input type="checkbox">` : `<input>`;
         answers_box.appendChild(new_answer);
     }
 
@@ -49,13 +49,13 @@ function CreateTest(){
     const is_public = settings.querySelector("input:checked") !== null;
     let questionsArray = [];
     for(const question of questions.querySelectorAll('.question')){
-        let question_container = question.querySelector(#${question.id}_container);
+        let question_container = question.querySelector(`#${question.id}_container`);
         const type_question = (question_container.querySelector("label").textContent == "Вопрос с выбором ответа:") ? "Выбор" : "Ввод";
         let question_grade = question.querySelector(".grade_question").value;
         let arrayQuestion = [];
-        let question_text = question_container.querySelector(input[class='question_input']).value;
+        let question_text = question_container.querySelector(`input[class='question_input']`).value;
         arrayQuestion.push(question_text, type_question, question_grade);
-        for(let answer of question_container.querySelector(#${question.id}_answers).querySelectorAll("p")){
+        for(let answer of question_container.querySelector(`#${question.id}_answers`).querySelectorAll("p")){
             let answer_text = answer.querySelector(".question_answer").value;
             let checkbox = answer.querySelector("input[type='checkbox']");
             let is_true = (checkbox !== null) ? checkbox.checked : true;
